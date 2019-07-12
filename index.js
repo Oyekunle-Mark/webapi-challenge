71920bf19bacd16efd8261c41d0fcf2ed27e6d5c
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 08:28:21 +0100

    Used the cors, logger, morgan, compression and dotenv middlewares

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
 const server = require('./server');
 
-server.listen(5000, () => console.log('Server started on port 5000'));
+const { PORT } = process.env;
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(logger('dev'));
+server.use(helmet());
+server.use(compression());
 
 module.exports = server;
