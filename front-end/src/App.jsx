79c2e38a68b1085dfca22eabf1093244fd2cb9b3
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 17:08:12 +0100

    styled the App component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import ProjectContext from './context';
 import Projects from './components/ProjectLink';
 import Project from './components/ProjectRoute';
 import Header from './components/Header';
+
+const StyledApp = styled.div`
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -27,9 +37,11 @@ class App extends Component {
     return (
       <Router>
         <ProjectContext.Provider value={this.state}>
-          <Header />
-          <Route exact path="/" component={Projects} />
-          <Project />
+          <StyledApp>
+            <Header />
+            <Route exact path="/" component={Projects} />
+            <Project />
+          </StyledApp>
         </ProjectContext.Provider>
       </Router>
     );
