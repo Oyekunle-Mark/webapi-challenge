22c44d9ec6c4674d445709aaa81d5dee657bfebf
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 17:08:30 +0100

    Styled teh actions

--- front-end/src/components/Action.jsx
+++ front-end/src/components/Action.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { string, bool } from 'prop-types';
+import styled from 'styled-components';
+
+const StyledAction = styled.li`
+  margin: 10px 0;
+  list-style-type: none;
+
+  h5 {
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  h6 {
+    margin: 5px 0;
+    font-style: italic;
+  }
+`;
 
 function Action({ description, notes, completed }) {
   return (
-    <div>
-      <p>{description}</p>
+    <StyledAction>
+      <h5>{description}</h5>
       <p>{notes}</p>
-      <p>Status: {completed ? 'Completed' : 'Not completed'}</p>
-    </div>
+      <h6>Status: {completed ? 'Completed' : 'Not completed'}</h6>
+    </StyledAction>
   );
 }
 
