98a7fdc9acb085f1b5b71c93731c3e8ac103373c
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 10:56:09 +0100

    Validates the req body for a project

--- middleware/validateProject.js
+++ middleware/validateProject.js
@@ -16,6 +16,25 @@ function validateProjectId(req, res, next) {
     .catch(err => res.status(500).json(err));
 }
 
+function validateProjectDetails(req, res, next) {
+  const { name, description, completed } = req.body;
+
+  if (!name || !description)
+    return res
+      .status(400)
+      .json({ message: 'Body must contain a name and description field' });
+
+  if (typeof completed !== 'undefined') {
+    if (typeof completed !== 'boolean')
+      return res
+        .status(400)
+        .json({ message: 'Completed field must be a boolean value.' });
+  }
+
+  next();
+}
+
 module.exports = {
   validateProjectId,
+  validateProjectDetails,
 };
--- router/project/index.js
+++ router/project/index.js
@@ -5,9 +5,14 @@ const validators = require('../../middleware/validateProject');
 
 router.get('/', handlers.getProjects);
 router.get('/:id', validators.validateProjectId, handlers.getProjectById);
-router.post('/', handlers.createProject);
+router.post('/', validators.validateProjectDetails, handlers.createProject);
 router.delete('/:id', validators.validateProjectId, handlers.deleteProject);
-router.put('/:id', validators.validateProjectId, handlers.updateProject);
+router.put(
+  '/:id',
+  validators.validateProjectId,
+  validators.validateProjectDetails,
+  handlers.updateProject,
+);
 router.get(
   '/:id/actions',
   validators.validateProjectId,
