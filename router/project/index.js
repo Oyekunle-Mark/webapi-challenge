ebcdbe97f9b32e14bab820d65786f108c95840f2
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 09:17:59 +0100

    Endpoint to get a project by ID

--- router/project/index.js
+++ router/project/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const handlers = require('./routeHandlers');
 
 router.get('/', handlers.getProjects);
+router.get('/:id', handlers.getProjectById);
 
 module.exports = router;
--- router/project/routeHandlers.js
+++ router/project/routeHandlers.js
@@ -6,6 +6,15 @@ function getProjects(req, res) {
     .catch(err => res.status(500).json(err));
 }
 
+function getProjectById(req, res) {
+  const { id } = req.params;
+
+  Projects.get(id)
+    .then(projects => res.status(200).json(projects))
+    .catch(err => res.status(500).json(err));
+}
+
 module.exports = {
   getProjects,
+  getProjectById,
 };
