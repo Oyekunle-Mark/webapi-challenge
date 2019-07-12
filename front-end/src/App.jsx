c5ea2b221c0163653f54d51177473fb729a5808e
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 12:32:07 +0100

    The container component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-import SampleComponent from './components/SampleComponent';
+import ProjectContext from './context';
 
-const App = () => (
-  <div>
-    <h1>I am the container</h1>
-    <SampleComponent />
-  </div>
-);
+class App extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/projects')
+      .then(res => this.updateProjects(res));
+  }
+
+  updateProjects = projects =>
+    this.setState({
+      projects,
+    });
+
+  render() {
+    return (
+      <ProjectContext.Provider value={this.state}>
+        <h1>Project Manager</h1>
+      </ProjectContext.Provider>
+    );
+  }
+}
 
 export default App;
