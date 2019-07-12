0d2c6a6d86c94dd86c962af6f0f2276b36fa49bb
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-12 14:30:10 +0100

    The header

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+  <header>
+    <h1>Project Manager</h1>
+    <Link to="/">Home</Link>
+  </header>
+);
